7b5a16a2652f1adcf84cf23d3eba06a00f2acb64
Najma Abdi <anajmabdi@gmail.com>
2023-03-25 21:33:29 -0700

    add engineer to machines, edit, and delete actions and views

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -6,40 +6,92 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
-  public class MachinesController : Controller
-  {
-    private readonly FactoryContext _db;
-
-    public MachinesController(FactoryContext db)
+    public class MachinesController : Controller
     {
-      _db = db;
-    }
+        private readonly FactoryContext _db;
 
-    public ActionResult Index()
-    {
-      return View(_db.Machines.ToList());
-    }
+        public MachinesController(FactoryContext db)
+        {
+            _db = db;
+        }
 
-    public ActionResult Details(int id)
-    {
-      Machine thisMachine = _db.Machines
-                            .Include(machine => machine.JoinEntities)
-                            .ThenInclude(join => join.Engineer)
-                            .FirstOrDefault(machine => machine.MachineId == id);
-                            return View(thisMachine);   
-    }
+        public ActionResult Index()
+        {
+            return View(_db.Machines.ToList());
+        }
 
-    public ActionResult Create()
-    {
-      return View();
-    }
+        public ActionResult Details(int id)
+        {
+            Machine thisMachine = _db.Machines
+                                  .Include(machine => machine.JoinEntities)
+                                  .ThenInclude(join => join.Engineer)
+                                  .FirstOrDefault(machine => machine.MachineId == id);
+            return View(thisMachine);
+        }
 
-    [HttpPost]
-    public ActionResult Create(Machine machine)
-    {
-      _db.Machines.Add(machine);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Machine machine)
+        {
+            _db.Machines.Add(machine);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddEngineer(int id)
+        {
+            Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+            ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+            return View(thisMachine);
+        }
+
+        [HttpPost]
+        public ActionResult AddEngineer(Machine machine, int engineerId)
+        {
+#nullable enable
+            EngineerMachine? joinEntity = _db.EngineerMachines.FirstOrDefault(join => (join.EngineerId == engineerId && join.MachineId == machine.MachineId));
+
+#nullable disable
+            if (joinEntity == null && engineerId != 0)
+            {
+                _db.EngineerMachines.Add(new EngineerMachine() { EngineerId = engineerId, MachineId = machine.MachineId });
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Details", new { id = machine.MachineId });
+        }
+
+        public ActionResult Edit(int id)
+        {
+          Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+          return View(thisMachine);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Machine machine)
+        {
+          _db.Machines.Update(machine);
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+          Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+          return View(thisMachine);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+          Machine thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+
+          _db.Machines.Remove(thisMachine);
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+        }
     }
-  }
 }
